8504622a55b1fd0053f380b0d0365b0523db5c10
C113 <949536395@qq.com>
2025-04-14 19:32:04 +0800

    修复pwa权限认证问题

--- src/pwa.rs
+++ src/pwa.rs
@@ -21,25 +21,25 @@ pub fn init_pwa(config: &Config) -> Result<(), Box<dyn Error>> {
     file.read_to_string(&mut index_html)?;
 
     // insert mainfest
-    const MANIFEST_LINK: &str = "<link rel=\"manifest\" href=\"/pwa/manifest.json\">\n";
-    if index_html.contains(MANIFEST_LINK) {
-        return Ok(());
+    const MANIFEST_LINK: &str =
+        "<link crossorigin=\"use-credentials\" rel=\"manifest\" href=\"manifest.json\">\n";
+    if !index_html.contains(MANIFEST_LINK) {
+        const MANIFEST_MARKER: &str = r#"<meta charset="UTF-8" />"#;
+        let insert_index = index_html.find(MANIFEST_MARKER).ok_or("marker not find")?;
+        index_html.insert_str(insert_index, MANIFEST_LINK);
     }
 
-    const MANIFEST_MARKER: &str = r#"<meta charset="UTF-8" />"#;
-    let insert_index = index_html.find(MANIFEST_MARKER).ok_or("marker not find")?;
-    index_html.insert_str(insert_index, MANIFEST_LINK);
-
     //insert serviceWorker
-    let service_marker = "<body>\n\t<div id=\"init-screen\">";
-    let service_insert = "    <script>\n        if (typeof navigator.serviceWorker !== 'undefined') {\n            navigator.serviceWorker.register('/pwa/sw.js')\n        }\n        </script>\n";
-    if !index_html.contains(service_insert) {
-        let marker_prefix_len = "<body>\n\t".len();
+    const SERVICE_INSERT: &str = "    <script>\n        if (typeof navigator.serviceWorker !== 'undefined') {\n            navigator.serviceWorker.register('/pwa/sw.js')\n        }\n        </script>\n";
+    if !index_html.contains(SERVICE_INSERT) {
+        const SERVICE_MARKER: &str = "<body>\n\t<div id=\"init-screen\">";
+        const MARKER_PREFIX_LEN: usize = "<body>\n\t".len();
+
         let insert_index_base = index_html
-            .find(service_marker)
+            .find(SERVICE_MARKER)
             .ok_or("Failed to find marker in index.html")?;
-        let insert_index = insert_index_base + marker_prefix_len;
-        index_html.insert_str(insert_index, service_insert);
+        let insert_index = insert_index_base + MARKER_PREFIX_LEN;
+        index_html.insert_str(insert_index, SERVICE_INSERT);
     }
 
     let mut file = std::fs::File::create(&index)?;
